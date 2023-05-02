cb5a1f354f3b1430bd1f6a46c05a5b038e5a9f82
Abhilash Mandula <mandula.abhilash@gmail.com>
2023-05-02 11:12:44 +0530

    Printing PDF using puppeteer

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -2,11 +2,12 @@ import asyncHandler from "express-async-handler";
 import fs from "fs";
 import path from "path";
 import pdf from "pdf-creator-node";
+import puppeteer from "puppeteer";
 
 // @desc    Generate PDF
 // @route   GET /api/generate-pdf
 // @access  Admin
-const generatePDF = asyncHandler(async (req, res) => {
+const generatePDFUsingPDFCreatorNode = asyncHandler(async (req, res) => {
   try {
     const __dirname = path.resolve();
 
@@ -23,12 +24,7 @@ const generatePDF = asyncHandler(async (req, res) => {
     const options = {
       format: "A4",
       orientation: "portrait",
-      // border: "10mm",
       pageFormats: {
-        // A4: {
-        //   width: "210mm",
-        //   height: "297mm",
-        // },
         // A4: {
         //   width: "210mm",
         //   height: "297mm",
@@ -112,6 +108,75 @@ const generatePDF = asyncHandler(async (req, res) => {
     res.set({
       "Content-Type": "application/pdf",
       "Content-Disposition": 'attachment; filename="pdf-test.pdf"',
+    });
+    res.send(pdfBuffer);
+  } catch (err) {
+    res.status(500);
+    console.log(err);
+    throw new Error("Internal server error");
+  }
+});
+
+const generatePDF = asyncHandler(async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+
+    // create a new page
+    const page = await browser.newPage();
+
+    const __dirname = path.resolve();
+
+    let html = fs.readFileSync(
+      path.join(__dirname, "./templates/report.html"),
+      "utf-8"
+    );
+
+    const css = fs.readFileSync(
+      path.join(__dirname, "./public/styles/style.css"),
+      "utf8"
+    );
+
+    const imagesPath = `http://localhost:3002/images`;
+
+    console.log(imagesPath);
+
+    html = html.replace(/{{imagesPath}}/g, imagesPath);
+
+    html = html.replace("</head>", `<style>${css}</style></head>`);
+
+    // set your html as the pages content
+    await page.setContent(html, {
+      waitUntil: "domcontentloaded",
+      waitUntil: "networkidle0",
+    });
+
+    // Wait for all images to load
+    // await page.waitForSelector('img[src^="http://localhost:3002/images"]', {
+    //   visible: true,
+    // });
+
+    // set viewport for the page
+    await page.setViewport({ width: 1280, height: 800 });
+
+    // create a pdf buffer
+    const pdfBuffer = await page.pdf({
+      format: "A4",
+    });
+
+    // or a .pdf file
+    await page.pdf({
+      format: "A4",
+      path: `${__dirname}/my-fance-invoice.pdf`,
+    });
+
+    // close the browser
+    await browser.close();
+
+    res.set({
+      "Content-Type": "application/pdf",
+      "Content-Disposition": 'attachment; filename="my-fance-invoice.pdf"',
     });
     res.send(pdfBuffer);
   } catch (err) {
